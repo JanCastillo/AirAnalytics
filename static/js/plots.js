9e2bd3ccc8b8f2d997918715f941255929f65e27
Jpkemperor <jan_jacm123@hotmail.com>
2019-10-03 13:09:51 -0500

    static js now filters airline and adds traces

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,84 +1,90 @@
 var destination = d3.select("#destination");
 var day = d3.select("#day");
-var filter = d3.select("#filter")
+var airline = d3.select("#airline");
+
+var filter = d3.select("#filter");
+var add = d3.select("#add");
 
 filter.on("click", function(){
+
     let day_value = day.property("value");
     let dest_value = destination.property("value");
+    let airline_value = airline.property("value");
 
-    buildcharts(day_value, dest_value);
+    buildcharts(day_value, airline_value, dest_value);
+    
+});
+
+add.on("click", function(){
+
+    let day_value = day.property("value");
+    let dest_value = destination.property("value");
+    let airline_value = airline.property("value");
+
+    addtraces(day_value, airline_value, dest_value);
 
 });
 
-function buildcharts (day, destination)
+function buildcharts (day, line, dest)
 {
 
         daystring = `/day/${day}`
         console.log(daystring);
         d3.json(daystring).then((x) => {
 
-            let aeromexico = x.filter(y => y[5] === "AMX");
-            let amx = aeromexico.filter(z => z[1] === destination)
-            let amx_precios = amx.map(p => p[4]);
-            let amx_salidas = amx.map(s => s[0]);
-            let amx_destinos = amx.map(d => d[1]);
-            console.log(`AEROMEXICO: ${amx_precios}`);
-
-            let avianca = x.filter(y => y[5] === "AVI");
-            let avi = avianca.filter(z => z[1] === destination)
-            let avi_precios = avi.map(p => p[4]);
-            let avi_salidas = avi.map(s => s[0]);
-            let avi_destinos = avi.map(d => d[1]);
-            console.log(`AVIANCA: ${avi_precios}`);
-
-            let united = x.filter(y => y[5] === "UAL");
-            let ual = united.filter(z => z[1] === destination)
-            let ual_precios = ual.map(p => p[4]);
-            let ual_salidas = ual.map(s => s[0]);
-            let ual_destinos = ual.map(d => d[1]);
-            console.log(`UNITED: ${ual_precios}`);
+            let airline = x.filter(y => y[5] === line);
+            let air = airline.filter(z => z[1] === dest);
+            let air_precios = air.map(p => p[4]);
+            let air_salidas = air.map(s => s[0]);
 
             let trace1 = 
             {
-            x: amx_salidas,
-            y: amx_precios,
-            text: amx_destinos,
-            };
-
-            let trace2 = 
-            {
-            x: avi_salidas,
-            y: avi_precios,
-            text: avi_destinos,
-            };
-
-            let trace3 = 
-            {
-            x: ual_salidas,
-            y: ual_precios,
-            text: ual_destinos,
+            x: air_salidas,
+            y: air_precios,
+            name: day,
             };
 
             let layout1 = 
             {
             xaxis: {title: "Hora"},
-            yaxis: {title: "Precio (MXN)"},
-            height: 250,
-            width: 1500
-            };
-
-            let layout2 = 
-            {
-            xaxis: {title: "Hora"},
-            yaxis: {title: "Precio (USD)"},
-            height: 250,
+            yaxis: {title: "Precio (MXN / USD)"},
+            height: 600,
             width: 1500
             };
     
             Plotly.newPlot("plot1", [trace1], layout1);
-            Plotly.newPlot("plot2", [trace2], layout1);
-            Plotly.newPlot("plot3", [trace3], layout2);
 
         });
 
-};
+}
+
+function addtraces (day, line, dest)
+{
+    daystring = `/day/${day}`
+    console.log(daystring);
+    d3.json(daystring).then((x) => {
+
+        let airline = x.filter(y => y[5] === line);
+        let air = airline.filter(z => z[1] === dest);
+        let air_precios = air.map(p => p[4]);
+        let air_salidas = air.map(s => s[0]);
+
+        let trace1 = 
+        {
+        x: air_salidas,
+        y: air_precios,
+        name: day,
+        };
+
+        let layout1 = 
+        {
+        xaxis: {title: "Hora"},
+        yaxis: {title: "Precio (MXN / USD)"},
+        height: 600,
+        width: 1500
+        };
+
+        Plotly.plot("plot1", [trace1], layout1);
+
+    });
+}
